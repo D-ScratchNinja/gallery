934efb6d5296e0e66442adf14a7ade7ddf36e5ac
DNin01 <106490990+DNin01@users.noreply.github.com>
2026-02-03 08:26:08 -0800

    Update base image URL

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function addProjectToList(data) {
 
   // Image source
   aElem.firstElementChild.src =
-    `https://uploads.scratch.mit.edu/get_image/project/${data.id}_480x360.png`;
+    `https://uploads.scratch.mit.edu/projects/thumbnails/${data.id}.png`;
 
   // Readable description
   aElem.ariaLabel = data.title;
